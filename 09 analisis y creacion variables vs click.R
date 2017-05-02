93ca98aa7d6c641fe7d9ec2e3d7f3393982a8fcb
pmtempone <pmtempone@gmail.com>
2017-05-02 00:36:08 -0300

    modelos dm_cup

--- 09 analisis y creacion variables vs click.R
+++ 09 analisis y creacion variables vs click.R
@@ -4,6 +4,10 @@ library(ggplot2)
 library(dplyr)
 library(reshape2) 
 library(gridExtra) ## allow us to plot two plots in a row
+
+library(RPostgreSQL)
+library(DBI)
+
 options(scipen=999) # disable cientific notation
 
 
@@ -41,3 +45,25 @@ categ_analysis(data=train_t, input="genericProduct", target = "click")
 categ_analysis(data=train_t, input="unit", target = "click")
 
 categ_analysis(data=train_t, input="salesIndex", target = "click")
+
+categ_analysis(data=train_t, input="campaignIndex", target = "click")
+
+train_v4 <- train_v3 %>% mutate(perc_adflag=ifelse(adFlag==1,0.572,0.380),
+                              perc_availability=ifelse(availability==1,0.461,ifelse(availability==2,0.255,
+                                                                                    ifelse(availability==3,0.230,
+                                                                                           0.001))),
+                              perc_generic=ifelse(genericProduct==1,0.615,0.42),
+                              perc_unit=ifelse(unit=='M',0.479,ifelse(unit=='ML',0.456,ifelse(unit=='G',0.442,
+                                                                                              ifelse(unit=='ST',0.433,
+                                                                                                     ifelse(unit=='KG',0.4,
+                                                                                                            ifelse(unit=='L',0.387,
+                                                                                                                   ifelse(unit=='P',0.282,0.185))))))),
+                              perc_sales=ifelse(salesIndex==40,0.509,ifelse(salesIndex==52,0.396,ifelse(salesIndex==53,0.371,0.337))),
+                              perc_campaign=ifelse(campaignIndex=='B',0.651,ifelse(campaignIndex=='A',0.522,ifelse(campaignIndex=='',0.415,0.404))))
+
+
+dbWriteTable(con,"train_v4",train_v4,row.names=FALSE)
+
+
+train_t <- train_v4 %>% filter(day >= 1 & day< 61)
+test_t <- train_v4 %>% filter(day >= 61 & day< 92)
---
+++ 11 modelo train_v5.R
@@ -0,0 +1,128 @@
+#train v5
+
+library(dplyr)
+library(caret)
+library(randomForest)
+library(miscTools)
+library(ggplot2)
+library(rpart)
+library(rattle)
+library(pROC)
+library(funModeling)
+library(ranger)
+library(RPostgreSQL)
+library(DBI)
+
+# loads the PostgreSQL driver
+drv <- dbDriver("PostgreSQL")
+# creates a connection to the postgres database
+# note that "con" will be used later in each connection to the database
+con <- dbConnect(drv, dbname = "DM_CUP",
+                 host = "localhost", port = 5432,
+                 user = "postgres", password = pw)
+
+train_v5 <- dbGetQuery(con, "SELECT * from train_v5")
+
+set.seed(307)
+
+variables <- c( 'competitorPrice', 'price','click',
+                'rrp','dia_semana','compar_compet','compar_ref','price_changed',
+                'orden_dia_producto','avg_comp_price','avg_price','lag_avg_comp_price','lag_avg_price','grupo_prod',
+                'cajas','kmeans_g','perc_adflag','perc_generic','perc_generic','perc_unit','perc_sales','perc_campaign','up_price',
+                'multiple','flag_avail','flag_generic','avail_generic')
+train_variables <- df_status(train_v5)
+
+train_v5$multiple <- factor(train_v5$multiple)
+train_v5$flag_avail <- factor(train_v5$flag_avail)
+train_v5$flag_generic <- factor(train_v5$flag_generic)
+train_v5$avail_generic <- factor(train_v5$avail_generic)
+
+train_t <- train_v5 %>% filter(day >= 1 & day< 61)
+test_t <- train_v5 %>% filter(day >= 61 & day< 92)
+
+head(train_t[,variables])
+
+
+sum(train_variables$variable %in% variables)
+
+train_t$click <- factor(train_t$click)
+fit.rpart <- rpart(click ~ .,data = train_t[,variables],control = rpart.control(cp=0.001,minsplit = 10,minbucket = round(20/3),maxdepth = 20),parms = list(split='gini'))
+
+fancyRpartPlot(fit.rpart)
+
+
+pred.rpart_click.v2 <- predict(fit.rpart,test_t[,variables],type = "prob")
+
+pred.rpart_click.v2 <- ifelse(pred.rpart_click.v2[,2] >= .57, 1,0)
+
+table("predicho"=pred.rpart_click.v2, "observado"=test_t[,"click"])
+
+
+g <- roc(click ~ pred.rpart_click.v2, data = test_t[,variables])
+plot(g, col="red") 
+g
+probpred.training <- predict(fit.rpart, type=c("prob"))
+probpred.training <- ifelse(probpred.training[,2] >= .5, 1,0)
+
+g.training <- roc(click ~ probpred.training, data = train_t)
+lines(g.training, col="blue") 
+legend("bottomright", c("entrenamiento","prueba"), col = c("blue", "red"), lty = 1 )
+
+#curva roc 61,59%, mejoro con respecto a modelo anterior
+
+#random forest
+
+set.seed(222)
+
+train_t$competitorPrice[is.na(train_t$competitorPrice)] <- 0
+train_t$compar_compet[is.na(train_t$compar_compet)] <- 0
+train_t$avg_comp_price[is.na(train_t$avg_comp_price)] <- 0
+train_t$lag_avg_comp_price[is.na(train_t$lag_avg_comp_price)] <- 0
+train_t$lag_avg_price[is.na(train_t$lag_avg_price)] <- 0
+train_t$dia_semana <- factor(train_t$dia_semana)
+
+
+fit_ranger <- ranger(click ~ ., data = train_t[,variables] , num.trees=100, min.node.size=10, probability=TRUE,importance = 'impurity' ) 
+sort(fit_ranger$variable.importance)
+
+var_ranger <- as.data.frame(fit_ranger$variable.importance)
+
+colnames(var_ranger) <- 'importancia'
+var_ranger$variable <- rownames(var_ranger)
+
+ggplot(var_ranger %>% arrange(desc(importancia)) %>% filter(importancia>18000),aes(x=variable,y=importancia))+geom_col()
+
+#fit.rf_click <- randomForest(click ~ .,data = train_t[,variables],ntree=100,importance=TRUE,maxnodes=25)
+
+
+test_t$competitorPrice[is.na(test_t$competitorPrice)] <- 0
+test_t$compar_compet[is.na(test_t$compar_compet)] <- 0
+test_t$avg_comp_price[is.na(test_t$avg_comp_price)] <- 0
+test_t$lag_avg_comp_price[is.na(test_t$lag_avg_comp_price)] <- 0
+test_t$lag_avg_price[is.na(test_t$lag_avg_price)] <- 0
+test_t$dia_semana <- factor(test_t$dia_semana)
+
+pred.rf_click <- predict(fit_ranger,test_t[,variables],type = "response")
+
+pred.rf_click <- ifelse(pred.rf_click$predictions[,2] >= .55, 1,0)
+
+table("predicho"=pred.rf_click, "observado"=test_t[,"click"])
+
+
+g <- roc(click ~ pred.rf_click, data = test_t)
+plot(g, col="red") 
+g
+
+probpred.training <- predict(fit_ranger,train_t[,variables],type = "response")
+probpred.training <- ifelse(probpred.training$predictions[,2] >= .57, 1,0)
+
+g.training <- roc(click ~ probpred.training, data = train_t)
+lines(g.training, col="blue") 
+legend("bottomright", c("entrenamiento","prueba"), col = c("blue", "red"), lty = 1 )
+
+remove(list = c("g.training","g","fit_ranger","pred.rpart_click","pred.rpart_click.v2","probpred.training","fit.rpart"))
+
+#65% CURVA roc, igual que la anterior prueba
+
+
+#xgboost
